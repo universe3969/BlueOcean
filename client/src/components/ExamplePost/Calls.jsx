56df2c80c6ace070b8c89324e6eb4f6bf41c511f
formalmiguel <mig.alvarez92@gmail.com>
2023-03-06 19:00:21 -0600

    Examples for how Auth0 is used.

---
+++ client/src/components/ExamplePost/Calls.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
+
+
+// this should be used only as an example view for how Auth0 work/ not apart of final product
+const Calls = () => {
+  const {getAccessTokenSilently} = useAuth0();
+  const user = useAuth0().user;
+
+  async function callApi() {
+    axios
+      .get("http://localhost:3000/regular")
+      .then((data) => console.log(data.data));
+  }
+
+  async function callProtectedApi() {
+    const token = await getAccessTokenSilently();
+    const response = await axios.get("http://localhost:3000/private",{
+      headers: {
+        authorization: `Bearer ${token}`
+      },
+      params: {
+        // we can use params to make specific calls to userid via email
+        user: user
+      }
+    })
+      .then((data) => console.log(data.data));
+  }
+
+  return (
+    <>
+      <button onClick={callApi}>Call API</button>
+      <br />
+      <button onClick={callProtectedApi}>Call Protected API</button>
+    </>
+  );
+};
+
+
+export default Calls;
