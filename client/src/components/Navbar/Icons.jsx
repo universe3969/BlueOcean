309e319fb9e77b1953633dabc78823ce3c9ae916
Shennie <shengfeng.wu.0308@gmail.com>
2023-03-07 10:42:25 -0500

    Finish Styling of Navigation Bar

---
+++ client/src/components/Navbar/Icons.jsx
@@ -0,0 +1,20 @@
+// Import all required icons
+import { 
+  IoLibrarySharp as Logo, IoSettingsOutline as Settings
+} from 'react-icons/io5'; 
+import { 
+  MdRateReview as Posts, MdExplore as Explore, 
+  MdOutlineMoreHoriz as More, MdAdd as Add,
+  MdOutlineLightMode as LightMode, MdOutlineDarkMode as DarkMode
+} from 'react-icons/md';
+import {
+  FaUserFriends as Friends, FaBook as Books, FaHome as Home,
+  FaUser as Profile, FaSms as Messages
+} from 'react-icons/fa';
+
+const Icons = {
+  Logo, Home, Books, Friends, Posts, Messages, Profile, Explore,
+  Add, Settings, DarkMode, LightMode, More
+};
+
+export default Icons;
