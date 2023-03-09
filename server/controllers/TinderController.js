fcc7de30cbabdbc76426d0bd49dd6bde2c35fac4
JaredFunaro <jaredfunaro@gmail.com>
2023-03-09 11:31:44 -0500

    tinder updates

--- server/controllers/TinderController.js
+++ server/controllers/TinderController.js
@@ -41,8 +41,25 @@ router.get('/book-genres', async(req, res) => {
 
   try {
     console.log('querying options');
+    console.log(req.params);
+    console.log(req.query);
+
+    // iterate thru the params construct a query string
+
     const result = await pool.query(
-      `SELECT * FROM books_genres LIMIT 5000`
+    //   `SELECT b.id, title, author, publish_date, page_count, description, cover_image, price, availability
+    //   FROM books b
+    // INNER JOIN books_genres bg ON bg.book_id = b.id
+    // INNER JOIN genres g ON g.id = bg.genre_id
+    // WHERE g.genre = 'Young Adult';`
+    // `SELECT b.id, bg.genre_id
+    // FROM books b
+    // JOIN books_genres bg ON bg.book_id = bg.book_id
+    // JOIN genres g ON bg.genre_id = bg.genre_id`
+
+    `
+    select books.id, books.cover_image, books.title, books.description, books.author, books_genres.book_id, books_genres.genre_id from books JOIN books_genres ON books.id = books_genres.book_id
+    `
     )
     .then((data) => {
       console.log('sending genres and books table');
