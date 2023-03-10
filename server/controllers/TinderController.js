59b3f874115130700ec458658f07a92625c1a204
JaredFunaro <jaredfunaro@gmail.com>
2023-03-09 19:29:33 -0500

    almost fully complete tinder, just need to add book db funcitonality

--- server/controllers/TinderController.js
+++ server/controllers/TinderController.js
@@ -37,39 +37,30 @@ router.get('/genre-options', async(req, res) => {
   }
 })
 
-router.get('/book-genres', async(req, res) => {
-
-  try {
-    console.log('querying options');
-    console.log(req.params);
-    console.log(req.query);
-
-    // iterate thru the params construct a query string
-
-    const result = await pool.query(
-    //   `SELECT b.id, title, author, publish_date, page_count, description, cover_image, price, availability
-    //   FROM books b
-    // INNER JOIN books_genres bg ON bg.book_id = b.id
-    // INNER JOIN genres g ON g.id = bg.genre_id
-    // WHERE g.genre = 'Young Adult';`
-    // `SELECT b.id, bg.genre_id
-    // FROM books b
-    // JOIN books_genres bg ON bg.book_id = bg.book_id
-    // JOIN genres g ON bg.genre_id = bg.genre_id`
-
-    `
-    select books.id, books.cover_image, books.title, books.description, books.author, books_genres.book_id, books_genres.genre_id from books JOIN books_genres ON books.id = books_genres.book_id
-    `
-    )
-    .then((data) => {
-      console.log('sending genres and books table');
-      res.status(201).send(data);
-    });
-    res.json(result);
-  } catch (err) {
-    console.log('there was an error acquring genres');
+router.post('/genres', async(req, res) => {
+  try {  const labelsArray = req.body?.selectedOptions.map(option => option.label);
+    const placeholders = labelsArray.map((label, index) => `$${index + 1}`).join(', ');
+    const text = `
+    SELECT b.*
+    FROM books b
+    INNER JOIN books_genres bg ON bg.book_id = b.id
+    INNER JOIN genres g ON g.id = bg.genre_id
+    WHERE g.genre IN (${placeholders})
+  `
+    const values = labelsArray;
+    const response = await pool.query(text, values);
+    // console.log(response.rows)
+    res.json(response.rows);
   }
-})
+  catch(err) {
+      console.log(err)
+      res.json('not yet')
+    }
+  });
+
+
+
+
 
 
 module.exports = router;
